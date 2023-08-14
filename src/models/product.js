63ed50dc288b4b3ca2d14f7f70d280410fb9fef2
Fahmi Ramdhani <ramdhanifahmi@students.itb.ac.id>
2023-08-14 18:21:17 +0700

    feat - add Dummy data - Add uuid dependency

--- src/models/product.js
+++ src/models/product.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const productSchema = new mongoose.Schema({
     videoId: { type: String, required: true },
-    productId: { type: String, required: true },
+    productId: { type: String, default: uuidv4, required: true },
     linkProduct: { type: String, required: true },
     title: { type: String, required: true },
     price: { type: Number, required: true },
@@ -15,3 +16,4 @@ const productSchema = new mongoose.Schema({
 const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
+
